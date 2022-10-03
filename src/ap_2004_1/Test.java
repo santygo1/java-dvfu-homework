87958b2bdece8dc866d314d2ea06ac3a2d51747d
Spirin Danil <danilkaspirin@gmail.com>
2022-10-03 12:30:33 +1000

    Homework 1

--- src/ap_2004_1/Test.java
+++ src/ap_2004_1/Test.java
@@ -1,2 +1,42 @@
-package ap_2004_1;public class Test {
+package ap_2004_1;
+
+public class Test {
+
+    public static void main(String[] args) {
+        WordList animals = new WordList("cat", "mouse", "frog", "dog", "dog");
+
+        testNumWords(animals);
+        testRemoveWords(animals);
+    }
+
+    private static void testNumWords(WordList wordList){
+        System.out.println("=== test 1 ===");
+        System.out.println("description: search count of list word by length");
+
+        System.out.println("List: " + wordList);
+
+        System.out.println("4: " + wordList.numWordsOfLength(4));
+        System.out.println("3: " + wordList.numWordsOfLength(3));
+        System.out.println("2: " + wordList.numWordsOfLength(2));
+
+        System.out.println("==============");
+    }
+
+    private static void testRemoveWords(WordList wordList){
+        System.out.println("=== test 2 ===");
+        System.out.println("description: remove words from list by length");
+
+        System.out.println("List: " + wordList);
+
+        wordList.removeWordsOfLength(4);
+        System.out.println("4: " + wordList);
+
+        wordList.removeWordsOfLength(3);
+        System.out.println("3: " + wordList);
+
+        wordList.removeWordsOfLength(2);
+        System.out.println("2: " + wordList);
+
+        System.out.println("==============");
+    }
 }
--- src/ap_2004_1/WordList.java
+++ src/ap_2004_1/WordList.java
@@ -1,5 +1,40 @@
-package ap2004;
+package ap_2004_1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WordList {
-    
+
+    private final ArrayList<String> myList;
+
+    public WordList(String ... words){
+        myList = new ArrayList<>(Arrays.asList(words));
+    }
+
+    /**
+     * Returns the number of words that are exactly len letters long
+     * @param len desired length for words
+     * @return count of words, which has specified length
+     */
+    public int numWordsOfLength(int len){
+        int matchCount = 0;
+
+        for (String current : myList)
+            if (current.length() == len) matchCount++;
+
+        return matchCount;
+    }
+
+    /**
+     * Removes elements, which has specified length
+     * @param len length to remove elements
+     */
+    public void removeWordsOfLength(int len){
+        myList.removeIf(s -> s.length() == len);
+    }
+
+    @Override
+    public String toString() {
+        return myList.toString();
+    }
 }
